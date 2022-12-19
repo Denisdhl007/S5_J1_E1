50ab465ffa02c8c7f610c82143811cfb275bf6a9
John Doe <denis.henin@icloud.com>
2022-12-19 11:53:07 +0100

    1

---
+++ public/script/script.js
@@ -0,0 +1,60 @@
+// . Déclaration d'une variable, Concaténation et Addition
+// exo 
+let reponse = "J’aime les tartines et j’en mange"
+console.log(reponse);
+
+// exo 
+let frequence = "4 par jours"
+console.log(frequence);
+
+// exo 
+let phrase = `${reponse} ${frequence}`;
+console.log(phrase);
+
+// exo 
+/*
+let trois = 5
+let quatre = 35
+let sum = `${trois} ${quatre}`; 
+console.log(sum);
+*/
+
+let trois = 5
+let quatre = 35
+let sum = 5+35;
+console.log(sum);
+
+// exo 
+let sum2 = `${reponse} ${trois} par jour`; 
+console.log(sum2)
+
+
+// 2. La réaffectation
+// exo 
+let salut = `Salut tout le monde`
+console.log(salut)
+
+// exo
+let bye = `Au revoir! `
+console.log(bye)
+
+// exo
+let ensemble = `${salut} = ${bye}`;
+console.log(ensemble)
+
+let salut2 = `${bye}`;
+console.log(salut)
+
+
+//3. Opérateurs de grandeur, les égalités, la différence et les Booleans
+// exo
+
+let number_1 = 4;
+let number_2 = `6`;
+
+console.log(number_1 > number_2)
+console.log(number_1 < number_2)
+console.log(number_1 == number_2)
+console.log(number_1 != number_2)
+console.log(number_1 === number_2)
+console.log(number_1 !== number_2)
